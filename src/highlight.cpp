b54111e9fdab5eb22c1c8a515562139193bc931d
z76k <zkhan31@safacommunityschool.com>
2025-12-27 17:22:54 +0000

    Using simple text file for highlights

--- src/highlight.cpp
+++ src/highlight.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+std::string currentBookFile = "unknown_book.txt";
+
+// This will save your highlight to a simple text file
+void saveHighlightToFile(std::string text) {
+    std::ofstream outfile;
+    // 'app' means append - it adds to the end of the file
+    outfile.open("highlights.txt", std::ios_base::app); 
+    outfile << currentBookFile << " : " << text << "\n";
+    outfile.close();
+}
+
+void loadHighlights() {
+    // Just a placeholder for now to satisfy the compiler
+}
+
+void deleteHighlightAt(unsigned long index) {
+    // Just a placeholder for now
+}
+
+struct Highlight { int dummy; }; 
+std::vector<Highlight> getHighlightsForCurrentBook() {
+    return std::vector<Highlight>();
+}
+
 bool selecting = false;
 void startOrFinishHighlight(int dummy) {
+    selecting = !selecting;
     if (!selecting) {
-        selecting = true;
-    } else {
-        selecting = false;
+        saveHighlightToFile("New Highlight Added");
     }
 }
