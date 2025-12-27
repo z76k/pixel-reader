60e800c9d5052bfb603ce9b0a51d74b808fcd919
z76k <zkhan31@safacommunityschool.com>
2025-12-27 17:27:29 +0000

    Added the missing highlight functions

--- src/highlight.cpp
+++ src/highlight.cpp
@@ -1,26 +1,10 @@
-#include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
 
-std::string currentBookFile = "unknown_book.txt";
+std::string currentBookFile = "";
 
-// This will save your highlight to a simple text file
-void saveHighlightToFile(std::string text) {
-    std::ofstream outfile;
-    // 'app' means append - it adds to the end of the file
-    outfile.open("highlights.txt", std::ios_base::app); 
-    outfile << currentBookFile << " : " << text << "\n";
-    outfile.close();
-}
-
-void loadHighlights() {
-    // Just a placeholder for now to satisfy the compiler
-}
-
-void deleteHighlightAt(unsigned long index) {
-    // Just a placeholder for now
-}
+void loadHighlights() {}
+void deleteHighlightAt(unsigned long index) {}
 
 struct Highlight { int dummy; }; 
 std::vector<Highlight> getHighlightsForCurrentBook() {
@@ -30,7 +14,4 @@ std::vector<Highlight> getHighlightsForCurrentBook() {
 bool selecting = false;
 void startOrFinishHighlight(int dummy) {
     selecting = !selecting;
-    if (!selecting) {
-        saveHighlightToFile("New Highlight Added");
-    }
 }
